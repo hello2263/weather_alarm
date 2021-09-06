2d86f892bc50dcb810aa5e51d701a3a9356e8020
dogyu <hello2263@naver.com>
2021-09-06 15:11:11 +0900

    fix

--- weather_data.py
+++ weather_data.py
@@ -10,7 +10,10 @@ from datetime import datetime
 import json
 import pymysql
 import weather_db
+import weather_local
 # import pandas as pd
+
+
 
 count = 0
 
@@ -40,75 +43,91 @@ else:
 today_date = str(now.year)+'0'+str(now.month)+str(today_day)
 #####################################################
 
-
-
-##################################################### 파라미터 설정
-CallBackURL = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
-params = '?' + urlencode({
-    quote_plus("serviceKey"): 'XIjRFoewvUDp4EDhRpATADoatwElkiQ%2F1J0tDooGjBTKStjRtuW3Zu89iE9cBsK%2Bz299IJwkbaE%2F%2F7SzcVo2yA%3D%3D',
-    quote_plus("numOfRows"): '1000',
-    quote_plus('pageNo'): '1',
-    quote_plus('dataType'): 'JSON',
-    quote_plus('base_date'): today_date,
-    quote_plus('base_time'): today_time,
-    quote_plus('nx'): '59',
-    quote_plus('ny'): '125'
-})
+##################################################### 엑셀에서 사용자 위치 추출
+user_x, user_y = weather_local.find_user_location()
 #####################################################
 
 
-
-# URL 데이터 파싱
-request = Request(CallBackURL + unquote(params))
-# API를 통해 데이터 GET
-response_body = urlopen(request).read()
-# JSON으로 변환
-data = json.loads(response_body)
-item_data = data['response']['body']['items']['item']
-
-# Pandas_DataFrame으로 출력
-# table = pd.DataFrame(data['response']['body']['items']['item'])
-# print(table)
-
-weather_data = dict() # 조회한 오늘 날씨 정보
-
-##################################################### DB연결 및 기존 테이블 삭제
-db_connecting(db_user, db_password)
-cursor.execute("TRUNCATE weather_future_db")
-db.commit()
-print('Delete Success')
+##################################################### 엑셀에서 서울 구마다 x, y좌표 따기함수
+name, code, x, y = weather_local.find_location()
 #####################################################
 
+for num in len(name):
 
-##################################################### 테이블에 넣을 데이터 정제
-for item in item_data:
-    weather_data['날짜'] = item['fcstDate']
-    weather_data['시간'] = item['fcstTime']
-    if item['category'] =='TMP': # 기온체크
-        weather_data['기온'] = item['fcstValue']
-        count += 1
-    if item['category'] == 'POP': # 상태체크
-        weather_data['강수확률'] = item['fcstValue']
-        count += 1
-    if item['category'] == 'SKY': # 하늘체크
-        weather_code = item['fcstValue']
-        count += 1
-        if weather_code == '1':
-            weather_state = 'sunny'
-        elif weather_code == '3':
-            weather_state = 'cloudy'
-        elif weather_code == '4':
-            weather_state = 'gray'
-        else:
-            weather_state = 'none'
-        weather_data['하늘'] = weather_state
-    # 컬럼개수를 만족하는 데이터는 테이블에 삽입
-    if count == 3:
-        cursor.execute("INSERT INTO weather_future_db(date, time, tmp, rain, sky) VALUES ('"+
-                       weather_data['날짜']+"', '"+weather_data['시간']+"', '" +
-                       weather_data['기온']+"', '"+weather_data['강수확률']+"', '"+weather_data['하늘']+"')")
-        db.commit()
-        count = 0
+    ##################################################### 파라미터 설정
+    CallBackURL = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
+    params = '?' + urlencode({
+        quote_plus("serviceKey"): 'XIjRFoewvUDp4EDhRpATADoatwElkiQ%2F1J0tDooGjBTKStjRtuW3Zu89iE9cBsK%2Bz299IJwkbaE%2F%2F7SzcVo2yA%3D%3D',
+        quote_plus("numOfRows"): '1000',
+        quote_plus('pageNo'): '1',
+        quote_plus('dataType'): 'JSON',
+        quote_plus('base_date'): today_date,
+        quote_plus('base_time'): today_time,
+        quote_plus('nx'): location_x.pop(0),
+        quote_plus('ny'): location_y.pop(0)'
+    })
+    #####################################################
+
+
+    # URL 데이터 파싱
+    request = Request(CallBackURL + unquote(params))
+    # API를 통해 데이터 GET
+    response_body = urlopen(request).read()
+    # JSON으로 변환
+    data = json.loads(response_body)
+    item_data = data['response']['body']['items']['item']
+
+    # Pandas_DataFrame으로 출력
+    # table = pd.DataFrame(data['response']['body']['items']['item'])
+    # print(table)
+
+
+
+    # ##################################################### DB연결 및 기존 테이블 삭제
+    # db_connecting(db_user, db_password)
+    # cursor.execute("TRUNCATE weather_future_db")
+    # db.commit()
+    # print('Delete Success')
+    # #####################################################
+
+
+
+
+
+    ##################################################### 테이블에 넣을 데이터 정제
+    weather_data = dict() # 조회한 오늘 날씨 정보
+
+    for item in item_data:
+        weather_data['날짜'] = item['fcstDate']
+        weather_data['시간'] = item['fcstTime']
+        if item['category'] =='TMP': # 기온체크
+            weather_data['기온'] = item['fcstValue']
+            count += 1
+        if item['category'] == 'POP': # 상태체크
+            weather_data['강수확률'] = item['fcstValue']
+            count += 1
+        if item['category'] == 'SKY': # 하늘체크
+            weather_code = item['fcstValue']
+            count += 1
+            if weather_code == '1':
+                weather_state = 'sunny'
+            elif weather_code == '3':
+                weather_state = 'cloudy'
+            elif weather_code == '4':
+                weather_state = 'gray'
+            else:
+                weather_state = 'none'
+            weather_data['하늘'] = weather_state
+        # 컬럼개수를 만족하는 데이터는 테이블에 삽입
+        if count == 3:
+            cursor.execute("INSERT INTO ", +code[num], +, "(date, time, tmp, rain, sky) VALUES ('"+
+                        weather_data['날짜']+"', '"+weather_data['시간']+"', '" +
+                        weather_data['기온']+"', '"+weather_data['강수확률']+"', '"+weather_data['하늘']+"')")
+            db.commit()
+            count = 0
+
+
+
 print('Update Success')
 db.close()
 #####################################################
